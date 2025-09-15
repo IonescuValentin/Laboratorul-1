49e39fb0db6fa2fc09c1125f4d69da8715455989
IonescuValentin <valentin.ionescu@iis.utm.md>
2025-09-15 10:30:25 +0300

    Rezolvarre conflicte si integrare cod

--- src/timers/TimerManager.java
+++ src/timers/TimerManager.java
@@ -1,1 +1,14 @@
-public class TimerManager { private TimerInterval t1=new TimerInterval(); private TimerAtTime t2=new TimerAtTime(); private TimerWithPeriod t3=new TimerWithPeriod(); public void startAll(){ t1.start(); t2.start(); t3.start(); } } 
+package timers;
+
+public class TimerManager {
+    private TimerInterval t1 = new TimerInterval();
+    private TimerAtTime t2 = new TimerAtTime();
+    private TimerWithPeriod t3 = new TimerWithPeriod();
+
+    public void startAll() {
+        System.out.println("Starting all timers...");
+        t1.start();  // Punctul 1
+        t2.start();  // Punctul 2
+        t3.start();  // Punctul 3
+    }
+}
--- src/timers/TimerWithPeriod.java
+++ src/timers/TimerWithPeriod.java
@@ -1,1 +1,30 @@
-public class TimerWithPeriod { public void start() { System.out.println("TimerWithPeriod (punctul3) placeholder"); } } 
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class TimerWithPeriod {
+    private Timer timer;
+
+    
+    public void start(long delay, long period) {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            int counter = 0;
+
+            @Override
+            public void run() {
+                counter++;
+                System.out.println("Punctul 3: Executare repetata #" + counter + " (period = " + period + " ms)");
+                if (counter >= 5) { 
+                    timer.cancel();
+                    System.out.println("TimerWithPeriod: stopped after 5 runs.");
+                }
+            }
+        }, delay, period);
+    }
+
+    
+    public void start() {
+        start(1000, 3000); // delay 1s, period 3s
+    }
+}
